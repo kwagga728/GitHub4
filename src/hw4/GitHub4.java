2122dc1e0cd96de77a1fafccd7e2c29bdb74f93b
brett <brett@DonLish>
2025-01-30 09:11:29 -0500

    CreateRepository

---
+++ src/hw4/GitHub4.java
@@ -0,0 +1,20 @@
+package hw4;
+
+public class GitHub4 {
+	
+public static void main(String[] args) {
+		
+		//Primitive Type
+		
+		int age = 10;
+		
+		age = 22;
+		age = 33;
+		
+		double price = 10;
+		price = 22.22;
+		price = 33.33;
+	}
+	
+
+}
